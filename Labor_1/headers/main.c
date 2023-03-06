964cfe5bd0a72da549b869bd56477478ad2f3f78
David030822 <demeter.david0322@gmail.com>
2023-03-06 20:36:54 +0200

    Labor 1

---
+++ Labor_1/headers/main.c
@@ -0,0 +1,13 @@
+#include "functions.h"
+
+int main()
+{
+    printf("The sum of 3.5 and 6.7 is: %f\n", sum(3.5, 6.7));
+    printf("The minimum of 7.1, 3.3 and 6.8 is: %f\n", minimum(7.1, 3.3, 6.8));
+
+    int n, *pArray;
+    scanf("%d", &n);
+    allocateMemoryForArray2(n, &pArray);
+
+    return 0;
+}
