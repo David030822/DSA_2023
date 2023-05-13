b3c427ee4188757769c11dac7b9c5ff2f09f9a00
David030822 <demeter.david0322@gmail.com>
2023-05-13 19:07:26 +0300

    Labor10b

---
+++ Labor_10b/headers/bst.h
@@ -0,0 +1,21 @@
+//
+// Created by David on 2023. 05. 08..
+//
+
+#ifndef LABOR_10_B_BST_H
+#define LABOR_10_B_BST_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "constants.h"
+#include <stdbool.h>
+#include <string.h>
+
+typedef struct Node{
+    int data;
+    struct Node *left, *right;
+}Node;
+
+
+
+#endif //LABOR_10_B_BST_H
---
+++ Labor_10b/sources/bst.c
@@ -0,0 +1,6 @@
+//
+// Created by David on 2023. 05. 08..
+//
+
+#include "../headers/bst.h"
+
